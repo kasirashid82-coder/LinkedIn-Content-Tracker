fca6dfd0d23d729586928130f99e31b4d136869c
Rashid Kasi <kasirashid82@gmail.com>
2026-02-20 14:09:21 +0000

    form.addEventListener("submit", function(event){     event.preventDefault();     console.log("Form submitted")  }

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
  const form = document.querySelector("form")
  console.log(form)
+
+ form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("Form submitted")
+ }
