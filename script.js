2fc875843f0b8aefae1a9d9dcf37f8495960264a
Rashid Kasi <kasirashid82@gmail.com>
2026-02-20 14:59:36 +0000

    form.addEventListener("submit", function(event)  {     event.preventDefault();
          const postDate = document.querySelector("#post-date").value;
        const hook = document.querySelector("#hook").value;
        const topic = document.querySelector("#topic").value;
        const reach = document.querySelector("#reach").value;
        const engagement = document.querySelector("#engagement").value;
        const connectionCount = document.querySelector("#connection-count").value;
    
        console.log(postDate, hook, topic, reach, engagement, connectionCount);
    });
    {
        "liveServer.settings.port": 5501
    }

--- script.js
+++ script.js
@@ -3,5 +3,14 @@
 
  form.addEventListener("submit", function(event)  {
     event.preventDefault();
-    console.log("Form submitted");
- });
+
+      const postDate = document.querySelector("#post-date").value;
+    const hook = document.querySelector("#hook").value;
+    const topic = document.querySelector("#topic").value;
+    const reach = document.querySelector("#reach").value;
+    const engagement = document.querySelector("#engagement").value;
+    const connectionCount = document.querySelector("#connection-count").value;
+    
+    console.log(postDate, hook, topic, reach, engagement, connectionCount);
+});
+
